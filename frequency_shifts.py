b2fb4cdea186a4d3599e6bb6a96a23affbebb2a9
MadMaxOfCitadel <anilnuru@gmail.com>
2018-12-01 08:11:16 -0500

    Advent of Code 2018 - Frequency Shifts

---
+++ frequency_shifts.py
@@ -0,0 +1,22 @@
+from functools import reduce
+
+frequencies_file = open('data\input1.txt', 'r')
+
+def getFinalFrequency():
+    print(reduce(lambda x, y : x + y, map(lambda x: int(x), frequencies_file.readlines())))
+
+def getFirstRepeatedFrequency():
+    freq_dict = {0: 1}
+    cum_freq = 0
+    frequencies = list(map(lambda x: int(x), frequencies_file.readlines()))
+    findx = 0
+    while True:
+        cum_freq += frequencies[findx % len(frequencies)]
+        if (cum_freq in freq_dict):
+            print(cum_freq)
+            break
+        else:
+            freq_dict[cum_freq] = 1
+        findx = findx + 1
+
+getFirstRepeatedFrequency()
